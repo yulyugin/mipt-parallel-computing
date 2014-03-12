845a250058d063725425b4531d8e2beb47112bdf
yulyugin <yulyugin@gmail.com>
2014-03-12 15:48:13 +0400

    Added demo of race condition.

---
+++ Lectures/lecture03-posix-threads/demo/race-condition.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+const int N = 20000;
+int x;
+
+static void *thread_func(void *args) {
+  int i;
+  for(i = 0; i < N; ++i) {
+    x ++;
+    //usleep(100);
+  }
+
+  return NULL;
+}
+
+int main (int argc, char **argv) {
+  int i;
+  pthread_t thread;
+
+  pthread_create(&thread, NULL, &thread_func, NULL);
+
+  for(i = 0; i < N; ++i) {
+    if (x%2 == 0)
+      printf("%d\n", x%2);
+  }
+
+  pthread_join(thread, NULL);
+
+  exit(EXIT_SUCCESS);
+}
