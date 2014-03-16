1eb2fdd291903da1da402185661350fa01e2ef67
yulyugin <yulyugin@gmail.com>
2014-03-17 01:11:47 +0400

    Finished first stupid version of graph builder.

--- Tools/builder.sh
+++ Tools/builder.sh
@@ -1,11 +1,19 @@
 #/bin/sh
 
-if [ $# != 1 ]
+if [ $# == 0 ]
 then
   echo "Invalid input format."
   echo "Usage: $0 [file name]"
   exit 1
 fi
+
+for i in $@
+do
+  if [ $i != $1 ]
+  then
+    params+="$i "
+  fi
+done
 
 mpicc -Wall $1
 
@@ -13,9 +21,15 @@ output="graph_input.tmp"
 
 echo "# process-count time" > $output
 
-for i in 1 2 4 8 16 32
+for i in 1 2 4 8 16 32 64 128
 do
-echo $i `mpirun -np $i a.out $N |grep Time | sed 's/[^0-9.]//g'` >> $output
+echo $i `mpirun -np $i a.out $params |grep Time | sed 's/[^0-9.]//g'` >> $output
 done
+
+export graph_input=$output
+export x_label="x"
+export y_label="y"
+
+cat builder.graph |gnuplot
 
 exit 0
