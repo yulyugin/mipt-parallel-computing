60d2f8a9f824f06c687762b38b94428f009c69ab
yulyugin <yulyugin@gmail.com>
2014-09-13 01:46:22 +0400

    Using standart C rand() function if RDRAND instruction is unavailable.

--- tools/rdrand.c
+++ tools/rdrand.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main () {
-    uint64_t rand = 0;
+    uint64_t random = 0;
     uint32_t ecx = 0;
     uint32_t leaf = 1, subleaf = 0;
 
-    // Check if RDRAND available in the CPU
+    /* Check if RDRAND available in the CPU */
     __asm__ __volatile__(
             "cpuid\n\t"
             :"=c"(ecx)
             :"a"(leaf), "c"(subleaf)
             :"ebx", "edx"
             );
-    // CPUID.01H:ECX.RDRAND[bit 30]
-    if (!(ecx && (1 << 30))) {
-        fprintf(stderr, "RDRAND instruction unavailable");
-        return 0;
+
+    if (!(ecx && (1 << 30))) { /* CPUID.01H:ECX.RDRAND[bit 30] */
+        fprintf(stderr, "RDRAND instruction is unavailable."
+                        " Using standart C rand() function.\n");
+        srand(time(NULL)); /* Initialize random number generator */
+        random = rand();
+    } else {
+        __asm__ __volatile__(
+                "rdrand %%rax\n\t"
+                :"=a"(random)
+                ::
+                );
     }
 
-    __asm__ __volatile__(
-            "rdrand %%rax\n\t"
-            :"=a"(rand)
-            ::
-            );
-
-    fprintf(stdout, "0x%llx\n", rand);
+    fprintf(stdout, "0x%llx\n", random);
     return 0;
 }
