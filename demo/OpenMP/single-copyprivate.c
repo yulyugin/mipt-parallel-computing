49b8923c1cc79042147e619f97f26e9e1bd269fc
yulyugin <yulyugin@gmail.com>
2014-09-19 01:36:50 +0400

    Add an example on simgle directive with copyprivate option

---
+++ demo/OpenMP/single-copyprivate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int i;
+    #pragma omp parallel private(i)
+    {
+        int i = omp_get_thread_num();
+        printf("Before single section: %d\n", i);
+        #pragma omp single copyprivate(i)
+            i = 100500;
+        printf("After single section: %d\n", i);
+    }
+
+    return 0;
+}
