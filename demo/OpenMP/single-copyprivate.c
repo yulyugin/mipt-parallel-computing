fdb3d0b915144f3312d37800dfd70b5997d3c496
yulyugin <yulyugin@gmail.com>
2014-09-19 19:03:13 +0400

    Update single section example.

--- demo/OpenMP/single-copyprivate.c
+++ demo/OpenMP/single-copyprivate.c
@@ -8,7 +8,10 @@ int main() {
         int i = omp_get_thread_num();
         printf("Before single section: %d\n", i);
         #pragma omp single copyprivate(i)
+        {
+            printf("Execute single section\n");
             i = 100500;
+        }
         printf("After single section: %d\n", i);
     }
 
