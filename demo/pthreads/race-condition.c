3aba09600eef7aeecc1562ae0fa055b77f2bee82
yulyugin <yulyugin@gmail.com>
2015-02-23 18:57:24 +0300

    Code style fixes only.

--- demo/pthreads/race-condition.c
+++ demo/pthreads/race-condition.c
@@ -7,26 +7,26 @@ const int N = 20000;
 int x;
 
 static void *thread_func(void *args) {
-  int i;
-  for(i = 0; i < N; ++i) {
-    x ++;
-  }
+    int i;
+    for(i = 0; i < N; ++i) {
+        x++;
+    }
 
-  return NULL;
+    return NULL;
 }
 
 int main (int argc, char **argv) {
-  int i;
-  pthread_t thread;
+    int i;
+    pthread_t thread;
 
-  pthread_create(&thread, NULL, &thread_func, NULL);
+    pthread_create(&thread, NULL, &thread_func, NULL);
 
-  for(i = 0; i < N; ++i) {
-    if (x%2 == 0)
-      printf("%d\n", x%2);
-  }
+    for(i = 0; i < N; i++) {
+        if (x%2 == 0)
+            printf("%d\n", x%2);
+    }
 
-  pthread_join(thread, NULL);
+    pthread_join(thread, NULL);
 
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
--- demo/pthreads/synchronized.c
+++ demo/pthreads/synchronized.c
@@ -8,31 +8,31 @@ int x;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void *thread_func(void *args) {
-  int i;
-  for(i = 0; i < N; ++i) {
-    pthread_mutex_lock(&mutex);
-    x ++;
-    pthread_mutex_unlock(&mutex);
-  }
+    int i;
+    for(i = 0; i < N; i++) {
+        pthread_mutex_lock(&mutex);
+        x++;
+        pthread_mutex_unlock(&mutex);
+    }
 
   return NULL;
 }
 
 int main (int argc, char **argv) {
-  int i;
-  pthread_t thread;
+    int i;
+    pthread_t thread;
 
-  pthread_create(&thread, NULL, &thread_func, NULL);
+    pthread_create(&thread, NULL, &thread_func, NULL);
 
-  for(i = 0; i < N; ++i) {
-    pthread_mutex_lock(&mutex);
-    if (x%2 == 0)
-      printf("%d\n", x%2);
-    pthread_mutex_unlock(&mutex);
-  }
+    for(i = 0; i < N; i++) {
+        pthread_mutex_lock(&mutex);
+        if (x%2 == 0)
+            printf("%d\n", x%2);
+        pthread_mutex_unlock(&mutex);
+    }
 
-  pthread_join(thread, NULL);
-  pthread_mutex_destroy(&mutex);
+    pthread_join(thread, NULL);
+    pthread_mutex_destroy(&mutex);
 
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
