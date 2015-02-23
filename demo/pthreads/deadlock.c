19595ea2e8549691d5927966b5601f96cc2bc50b
yulyugin <yulyugin@gmail.com>
2015-02-23 18:55:01 +0300

    Make deadlock example fluky.

--- demo/pthreads/deadlock.c
+++ demo/pthreads/deadlock.c
@@ -7,36 +7,33 @@ pthread_mutex_t A = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t B = PTHREAD_MUTEX_INITIALIZER;
 
 static void *thread_func(void *args) {
-  usleep(100);
-  pthread_mutex_lock(&A);
-  usleep(100);
-  pthread_mutex_lock(&B);
-  usleep(100);
-  pthread_mutex_unlock(&B);
-  pthread_mutex_unlock(&A);
+    pthread_mutex_lock(&A);
+    pthread_mutex_lock(&B);
+    pthread_mutex_unlock(&B);
+    pthread_mutex_unlock(&A);
 
-  return NULL;
+    return NULL;
 }
 
 int main (int argc, char **argv) {
-  pthread_t thread;
+    pthread_t thread;
+    srand(time(NULL)); /* Initialize random number generator. */
 
-  pthread_create(&thread, NULL, &thread_func, NULL);
+    pthread_create(&thread, NULL, &thread_func, NULL);
 
-  usleep(100);
-  pthread_mutex_lock(&B);
-  usleep(100);
-  pthread_mutex_lock(&A);
-  usleep(100);
-  pthread_mutex_unlock(&A);
-  pthread_mutex_unlock(&B);
+    pthread_mutex_lock(&B);
+    if (rand()%100 < 20)
+        usleep(100);
+    pthread_mutex_lock(&A);
+    pthread_mutex_unlock(&A);
+    pthread_mutex_unlock(&B);
 
-  pthread_join(thread, NULL);
+    pthread_join(thread, NULL);
 
-  printf("Success\n");
+    printf("Success\n");
 
-  pthread_mutex_destroy(&A);
-  pthread_mutex_destroy(&B);
+    pthread_mutex_destroy(&A);
+    pthread_mutex_destroy(&B);
 
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
